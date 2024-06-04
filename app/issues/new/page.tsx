fd955d72d3a4174827a38953027d42212ee3e204
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-04 14:14:35 -0700

    fixed navigator error in terminal, set up newIssuepage further

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,35 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import dynamic from "next/dynamic";
+import { Button, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+interface NewIssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  //This allows us to register our input fields with react-hook-form
+  const { register, control } = useForm<NewIssueForm>();
+
   return (
     <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description"></TextArea>
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Describe your issue here..." {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
     </div>
   );
