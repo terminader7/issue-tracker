3a24a321078236818ec20668ca8a48330adffe6e
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-04 12:31:01 -0700

    added more styling to the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { RiBug2Fill } from "react-icons/ri";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log({ currentPath });
+
   const links = [
     { name: "Dashboard", href: "/" },
     { name: "Issues", href: "/issues" },
@@ -9,13 +16,17 @@ const NavBar = () => {
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
-        <RiBug2Fill />
+        <RiBug2Fill className="w-6 h-6" />
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
           <li key={link.href}>
             <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
               href={link.href}
             >
               {link.name}
