2d296fa4c231e8a97081f49478f917e4790fa56c
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-04 14:43:03 -0700

    added error handling when user tries to submit a new issue

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -4,8 +4,8 @@ import { z } from "zod";
 
 // Only need the title and the description because the server is generating the id, created and updated fields
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required"),
 });
 
 export async function POST(request: NextRequest) {
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import dynamic from "next/dynamic";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { MdError } from "react-icons/md";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
@@ -20,29 +22,43 @@ const NewIssuePage = () => {
 
   //This allows us to register our input fields with react-hook-form
   const { register, control, handleSubmit } = useForm<NewIssueForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios
-          .post("/api/issues", data)
-          .then(() => router.push("/issues"));
-      })}
-    >
-      <TextField.Root
-        placeholder="Title"
-        {...register("title")}
-      ></TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Describe your issue here..." {...field} />
-        )}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl space-y-3">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <MdError className="w-4 h-4" />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios
+              .post("/api/issues", data)
+              .then(() => router.push("/issues"));
+          } catch (error) {
+            setError("An Unexpected error occurred");
+          }
+        })}
+      >
+        <TextField.Root
+          placeholder="Title"
+          {...register("title")}
+        ></TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Describe your issue here..." {...field} />
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
